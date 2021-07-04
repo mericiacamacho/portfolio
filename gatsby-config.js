96352e1fc01118410d50668bc76cb5cd7053bfe5
Angela Camacho <angelamericiac@hotmail.com>
2021-07-04 19:14:53 +0100

    gatsby installed

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,27 +1,49 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "Angela Portfolio",
+    siteUrl: 'https://www.yourdomain.tld',
+    title: 'Angela Portfolio',
   },
   plugins: [
-    "gatsby-plugin-sass",
-    "gatsby-plugin-image",
+    'gatsby-plugin-sass',
+    'gatsby-plugin-image',
     {
-      resolve: "gatsby-plugin-google-analytics",
+      resolve: 'gatsby-plugin-google-analytics',
       options: {
-        trackingId: "G-PPBVWMLCES",
+        trackingId: 'G-PPBVWMLCES',
       },
     },
-    "gatsby-plugin-sitemap",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "images",
-        path: "./src/images/",
+        name: 'images',
+        path: './src/images/',
       },
-      __key: "images",
+      __key: 'images',
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+          fonts: [
+              `roboto:300`,       
+              `yomogi:400`,       
+          ], display: 'swap',      
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-eslint',
+      options: {
+        stages: ['develop'],
+        extensions: ['js', 'jsx'],
+        exclude: ['node_modules', '.cache', 'public'],
+        // Any eslint-webpack-plugin options below
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-prettier-eslint',
     },
   ],
 };
+
---
+++ src/components/Title.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Title() {
+    return (
+    <h1>
+        {children}
+    </h1>
+    )
+}
+
